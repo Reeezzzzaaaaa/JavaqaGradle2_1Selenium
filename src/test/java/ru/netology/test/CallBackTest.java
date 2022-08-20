1b4e8ee3fefaa14d64cb6279f6676c9817d6c5b4
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-08-21 01:31:06 +0300

    CallBackTest commit

--- src/test/java/ru/netology/test/CallBackTest.java
+++ src/test/java/ru/netology/test/CallBackTest.java
@@ -3,19 +3,19 @@ package ru.netology.test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.List;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CallBackTest {
 
@@ -39,11 +39,6 @@ class CallBackTest {
     @BeforeAll
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "/drivers/win/chromedriver.exe");
-    }
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
     }
 
     @AfterEach
