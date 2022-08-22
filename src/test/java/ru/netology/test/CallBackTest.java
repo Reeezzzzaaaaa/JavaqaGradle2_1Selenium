1e2b8950ee1ace524d1f7bb1ba05e58823b71ea3
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-08-22 21:02:37 +0300

    fixed commit

--- src/test/java/ru/netology/test/CallBackTest.java
+++ src/test/java/ru/netology/test/CallBackTest.java
@@ -1,14 +1,8 @@
 package ru.netology.test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -16,70 +10,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 class CallBackTest {
 
-    WebDriver driver;
-
-    void ChromeOptions() {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        options.addArguments("window-size=1920,1080");
-        driver = new ChromeDriver(options);
-    }
-
-    @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "/drivers/win/chromedriver.exe");
-    }
-
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        open("http://localhost:9999/");
     }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
-
-//    @Test
-//    void FirstOptionTest() {
-//
-//        driver.get("http://localhost:9999/");
-//        List<WebElement> inputs = driver.findElements(By.tagName("input"));
-//        inputs.get(0).sendKeys("Иванов-Петров Михаил");
-//        inputs.get(1).sendKeys("+79991234567");
-//        driver.findElement(By.className("checkbox__box")).click();
-//        driver.findElement(By.className("button__text")).click();
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    void SecondOptionTest() {
-//
-//        driver.get("http://localhost:9999/");
-//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов-Петров Михаил");
-//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79991234567");
-//        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button__text")).click();
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-//        assertEquals(expected, actual);
-//    }
 
     @Test
     void selenideOptionTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Иванов-Петров Михаил");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+79991234567");
         $(By.cssSelector("[data-test-id=agreement]")).click();
@@ -90,7 +28,6 @@ class CallBackTest {
     @Test
     void nameSpecialCharacterTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Иванов-Петров Михаил@");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+79991234567");
         $(By.cssSelector("[data-test-id=agreement]")).click();
@@ -101,7 +38,6 @@ class CallBackTest {
     @Test
     void nameEmptyTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+79991234567");
         $(By.cssSelector("[data-test-id=agreement]")).click();
@@ -112,7 +48,6 @@ class CallBackTest {
     @Test
     void nameEngTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Petrov Ivan");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+79991234567");
         $(By.cssSelector("[data-test-id=agreement]")).click();
@@ -123,7 +58,6 @@ class CallBackTest {
     @Test
     void phoneEmptyTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Иванов-Петров Михаил");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("");
         $(By.cssSelector("[data-test-id=agreement]")).click();
@@ -134,7 +68,6 @@ class CallBackTest {
     @Test
     void phone10SymbolTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Иванов-Петров Михаил");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+7999123456");
         $(By.cssSelector("[data-test-id=agreement]")).click();
@@ -145,7 +78,6 @@ class CallBackTest {
     @Test
     void phone12SymbolTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Иванов-Петров Михаил");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+799912345678");
         $(By.cssSelector("[data-test-id=agreement]")).click();
@@ -156,11 +88,10 @@ class CallBackTest {
     @Test
     void agreementTest() {
 
-        open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=name] input")).setValue("Иванов-Петров Михаил");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+79991234567");
         $(By.cssSelector("[data-test-id=agreement]"));
         $(By.className("button__text")).click();
-        $("[data-test-id=agreement].input_invalid .checkbox__box").shouldHave(empty);
+        $("[data-test-id=agreement].input_invalid .checkbox__box").shouldBe(visible);
     }
 }
